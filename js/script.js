4caa1b3b65673eff1b66c4b7f26f23b26f9db644
Isa Mary <isa.kusuki@gmail.com>
2022-04-29 14:24:18 -0300

    Adiciona click e alert em JS

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('MSG gerada no JS!')
+
+//Declarando variáveis com var
+//Utilizamos o var para criar um escopo global
+var nome = "Isa"
+
+//Declarando variáveis com let
+//Utilizamos o let para criar um escopo local
+let sobrenome = "Mary"
+
+//Criando constantes
+//Para criar constantes utilizamos a palavra chave const
+const elementoInput = document.querySelector("#telClienteId")
+
+//Atrelando um evento de mouse ao elemento, utilizando um EventListener
+//Seria algo como um escutador de eventos.
+//Ele recebe como parâmetros(Evento,função)
+elementoInput.addEventListener("click",function(){
+
+    alert('TESTE')
+
+})
