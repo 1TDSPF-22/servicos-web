2b972163cd6557713a5f193c8fc62e54f8b67bb0
Mateus-Cabral <RM93000@fiap.com.br>
2022-04-29 12:12:49 -0300

    formulário e script.js adicionados

---
+++ js/script.js
@@ -0,0 +1,29 @@
+console.log("Mensagem gerada no arquivo js")
+
+
+// Declarando variáveis com var
+// Utilizamos var para criar com escopo global
+
+var nome = "Hairo"
+
+// Declarando variáveis com let
+// Utilizamos let para criar com escopo local
+
+let sobrenome =  "Newdawn"
+
+// Criando constantes
+// Utilizamos const para criar constantes
+
+const elementoInput = document.querySelector("#telClienteId")
+
+// Atrelando um evento de mouse ao elemento, utilizando um EventListener
+// Um scanner de eventos
+// Recebe como parâmetros(<Evento (String)>, <função>)
+
+elementoInput.addEventListener("click", function(){
+
+    alert('AAAAAAAAAA')
+
+})
+
+    
