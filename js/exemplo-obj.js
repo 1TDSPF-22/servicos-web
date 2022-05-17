00398b0bf5b3db7680e7d56d284daa524c19910c
ViníciusYoda <rm92969@fiap.com.br>
2022-05-17 15:03:31 -0300

    obj

--- js/exemplo-obj.js
+++ js/exemplo-obj.js
@@ -10,8 +10,22 @@ var pessoa ={
     idade:17,
     interesses:['Aposentar', 'Viajar', 'Carro voador'],
     geral: function(){
-        console.log(`${this.nome} tem ${this.idade}`)
+        console.log(`${this.nome} tem ${this.idade} anos de idade. Ele tem interesse em ${this.interesses[0]}, ${this.interesses[1]} e ${this.interreses[2]}.`)
+    },
+
+    saudacao:function(){
+        alert(`Meu nome é ${this.nome}`)
     }
 }
 
-console.log(ppessoa)
+//Imprimindo o objeto
+//console.log(pessoa)
+
+//Acessando um atributo
+
+//console.log(pessoa.nome)
+console.log(pessoa["nome"])
+
+//Executado metodos do objeto
+pessoa.geral()
+pessoa.saudacao()
