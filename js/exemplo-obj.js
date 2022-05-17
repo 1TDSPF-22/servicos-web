ca145848cf8b045c44c0cec8d8900ccb6bf8330b
gabtreze <RM95603@fiap.com>
2022-05-17 09:56:38 -0300

    adicionado forms

---
+++ js/exemplo-obj.js
@@ -0,0 +1,55 @@
+
+// //Exemplo de criação Objeto em Javascript
+// var pessoa = {}
+// console.log(pessoa)
+
+//Exemplo de criação Objeto em Javascript
+// var pessoa = {
+//     nome:"Igor",
+//     idade:19,
+//     interesses:['futebol','musica','carros'],
+//     geral: function(){
+//         console.log(`${this.nome} tem ${this.idade} anos de idade. Ele gosta de ${this.interesses[0]}, ${this.interesses[1]} e ${this.interesses[2]} .`)
+//     },
+//     saudacao:function(){
+//         alert(`Oi eu sou ${this.nome}`)
+//     }
+// }
+
+
+// //Imprimindo o objeto
+// //console.log(pessoa)
+
+// //Acessando um atributo
+// // console.log(pessoa.nome)
+// console.log(pessoa["nome"])
+
+// //Executando métodos do objeto
+// pessoa.geral()
+// pessoa.saudacao()
+
+//INTANCIAR UM OBJETO
+var pessoa = {
+    nome:"",
+    email:""
+}
+
+//RECUPERANDO OS CAMPOS E POPULANDO UM NOVO OBJETO
+
+const botaoSubmit = document.getElementById("btnCadCli")
+
+//Atrelar listen ao evento de botão para que eu possa
+// recuperar os dados dos inputs(campos) assim que ele(botão) for clicado
+botaoSubmit.addEventListener("click", ()=>{
+    //Recuperando todos os inputs com querySelectorAll
+    const todosInputs = document.querySelectorAll("input")
+    pessoa.nome  = todosInputs[0].value
+    pessoa.email = todosInputs[1].value
+})
+
+//IMPRIMINDO O OBJETO COM OS DADOS RECUPERADOS!!
+console.log("NOME  : " + pessoa.nome)
+console.log("EMAIL : " + pessoa.email)
+
+
+
