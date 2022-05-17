3456822a63b114cb9585b31558a658306132e0be
ViníciusYoda <rm92969@fiap.com.br>
2022-05-17 11:51:05 -0300

    obj

---
+++ js/exemplo-obj.js
@@ -0,0 +1,17 @@
+
+//Exemplo de Objetos em Javascript
+//var pessoa = {}
+
+//console.log(pessoa)
+
+//Exemplo de criação Objeto em Javascript
+var pessoa ={
+    nome:"Vinícius",
+    idade:17,
+    interesses:['Aposentar', 'Viajar', 'Carro voador'],
+    geral: function(){
+        console.log(`${this.nome} tem ${this.idade}`)
+    }
+}
+
+console.log(ppessoa)
